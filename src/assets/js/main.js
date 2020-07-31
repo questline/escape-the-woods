8516e5d5ad384eb4290f7b5b5663890436639a06
viri.space <admin@viri.xyz>
2020-07-31 20:00:14 +0100

    updated brand name

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,7 +4,7 @@ Config.passages.nobr = true;
 
 // Adds the header to the page
 $('<div id="header"><h1>Questline</h1></div>').prependTo(document.body);
-$('<div id="footer"><h5>a <a href="https://vaynwork.com">Vaynwork</a> project<br>Visit the <a href="https://github.com/vaynwork/questline">GitHub repo</a> for this site</h5></></div>').appendTo(document.body);
+$('<div id="footer"><h5>a <a href="https://vuwnu.com">Vuwnu</a> project<br>Visit the <a href="https://github.com/vuwnu/questline">GitHub repo</a> for this site</h5></></div>').appendTo(document.body);
 
 //Adds custom head text
 $('head').append(tale.get("HeadAppend").text);
