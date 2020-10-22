de0823bed93bef99897d673e74ef9b1a2a435b94
viri.space <admin@viri.xyz>
2020-10-22 16:27:02 +0100

    updated header and footer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,8 +3,8 @@ UIBar.destroy();
 Config.passages.nobr = true;
 
 // Adds the header to the page
-$('<div id="header"><h1>Questline</h1></div>').prependTo(document.body);
-$('<div id="footer"><h5>a <a href="https://vuwnu.com">Vuwnu</a> project<br>Visit the <a href="https://github.com/vuwnu/questline">GitHub repo</a> for this site</h5></></div>').appendTo(document.body);
+$('<div id="header"><h1>Escape The Woods</h1></div>').prependTo(document.body);
+$('<div id="footer"><h5>Story brought to you by <a href="https://vuwnu.com">Vuwnu</a><br>Visit the <a href="https://github.com/questline/escape-the-woods">GitHub repo</a> for this site</h5></div>').appendTo(document.body);
 
 //Adds custom head text
 $('head').append(tale.get("HeadAppend").text);
