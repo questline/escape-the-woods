aed28ebf17b93f9b056ab5fa74d270a741b694a0
viri.space <admin@viri.xyz>
2020-10-22 17:36:30 +0100

    removed header and footer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,9 +2,5 @@
 UIBar.destroy();
 Config.passages.nobr = true;
 
-// Adds the header to the page
-$('<div id="header"><h1>Escape The Woods</h1></div>').prependTo(document.body);
-$('<div id="footer"><h5>Story brought to you by <a href="https://vuwnu.com">Vuwnu</a><br>Visit the <a href="https://github.com/questline/escape-the-woods">GitHub repo</a> for this site</h5></div>').appendTo(document.body);
-
 //Adds custom head text
 $('head').append(tale.get("HeadAppend").text);
